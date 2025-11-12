03aae95a42f9b80b2758b610032c2865fc0138fd
Muhammad Nur Alif <hiamalif@Muhammads-MacBook-Pro.local>
2025-11-12 12:47:59 +0700

    update rkg webprofile phase 2 clear

--- src/components/sections/event/LocaleDetailEvent.tsx
+++ src/components/sections/event/LocaleDetailEvent.tsx
@@ -13,7 +13,7 @@ export default function LocaleDetailEvent({
             alt={eventData?.title + `Hero Image`}
             className="block w-full object-cover max-h-[90vh] aspect-video"
          />
-         <section className="bg-black text-white">
+         <section className="bg-black text-white px-4">
             <div className="container mx-auto py-20">
                <h1 className="text-5xl font-bold mb-10">{eventData?.title}</h1>
                <div
@@ -27,7 +27,7 @@ export default function LocaleDetailEvent({
             </div>
          </section>
 
-         <section className="bg-black text-white">
+         <section className="bg-black text-white px-4">
             <div className="container mx-auto pb-20">
                <h2 className="text-5xl font-semibold mb-10">Gallery</h2>
                <GalleryMansory items={eventData?.galleries ? eventData.galleries.filter(gallery => gallery).map((media, i) => ({id: i, media, title: eventData.title, excerpt: eventData.description})) : []}/>
