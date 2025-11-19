dc5b67b4b79401cedf5aefe1eaee90094b18bef3
Muhammad Nur Alif <hiamalif@Muhammads-MacBook-Pro.local>
2025-11-19 14:34:44 +0700

    update with all easy configurate

--- admin/frontend/minimal-next/next.config.ts
+++ admin/frontend/minimal-next/next.config.ts
@@ -39,6 +39,10 @@ const nextConfig: NextConfig = {
             protocol: 'http',
             hostname: 'localhost',
          },
+         {
+            protocol: 'https',
+            hostname: 'api.rkgtour.com',
+         },
       ],
    },
 
